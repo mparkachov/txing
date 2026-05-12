e6965fd79003c9a754de5eb424dade7703a098f0
Maxim Parkachov <mparkachov@users.noreply.github.com>
2026-05-12 19:19:41 +0200

    Remove debug devices and prune common MCU to NCS

--- devices/ble-debug/rig/src/ble_debug_rig/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Host-side BLE debug tools."""
--- devices/rust-debug/rig/src/main.rs
+++
@@ -1,7 +0,0 @@
-#[tokio::main]
-async fn main() {
-    if let Err(err) = rust_debug_rig::cli::run().await {
-        eprintln!("{} {}", err.stage, err.message);
-        std::process::exit(2);
-    }
-}
