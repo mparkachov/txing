074d68b8192d7723c8f82c03b8871c30952d6c5f
Maxim Parkachov <mparkachov@users.noreply.github.com>
2026-05-23 08:58:22 +0200

    Bumped to release 0.11.1

--- devices/unit/board/kvs_master/include/kvs_master/version.hpp
+++ devices/unit/board/kvs_master/include/kvs_master/version.hpp
@@ -5,7 +5,7 @@
 
 namespace txing::board::kvs_master {
 
-inline constexpr std::string_view kTxingBoardKvsMasterVersion = "0.11.0";
+inline constexpr std::string_view kTxingBoardKvsMasterVersion = "0.11.1";
 
 }  // namespace txing::board::kvs_master
 
