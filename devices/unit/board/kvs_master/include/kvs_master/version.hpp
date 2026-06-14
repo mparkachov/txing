b3e13e99e4333edbf7cf4749610995de729ecebd
Maxim Parkachov <mparkachov@users.noreply.github.com>
2026-06-14 12:21:54 +0200

    Bumped unit to 0.14.12

--- devices/unit/board/kvs_master/include/kvs_master/version.hpp
+++ devices/unit/board/kvs_master/include/kvs_master/version.hpp
@@ -5,7 +5,7 @@
 
 namespace txing::board::kvs_master {
 
-inline constexpr std::string_view kTxingUnitKvsMasterVersion = "0.14.11";
+inline constexpr std::string_view kTxingUnitKvsMasterVersion = "0.14.12";
 
 }  // namespace txing::board::kvs_master
 
--- devices/unit/daemon/internal/daemon/version.go
+++ devices/unit/daemon/internal/daemon/version.go
@@ -1,5 +1,5 @@
 package daemon
 
-const packageVersion = "0.14.11"
+const packageVersion = "0.14.12"
 
 var DaemonVersion = packageVersion
