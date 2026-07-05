7702ff43f5b13dd94e7ce8c3bfd7bf9030b1d7e6
Maxim Parkachov <mparkachov@users.noreply.github.com>
2026-07-05 17:54:29 +0200

    Bumped all components to 0.15.4, local mac txing implemented

--- office/src/config.ts
+++ office/src/config.ts
@@ -34,7 +34,7 @@ const buildConfig = () => {
   const txingVersion =
     typeof __TXING_VERSION__ === 'string' && __TXING_VERSION__.trim()
       ? __TXING_VERSION__.trim()
-      : '0.15.0'
+      : '0.15.4'
 
   const errors: string[] = []
 
